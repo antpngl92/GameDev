0e2f9add13dba1566a2a90d592f0e24e7565d0ed
Gabriel Aduculesei <gabriel.aduculesei@gmail.com>
2020-12-04 19:26:31 +0000

    added volume bar functionality

---
+++ ScifiShooter/MultiPlatform Game Dev Project/Assets/Scripts/Game/SettingsMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public void AdjustVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+}
